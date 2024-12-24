9fbd603b087fcc7376e91a7979396d3a68a5c476
kevinlee <105219462+kiv9908@users.noreply.github.com>
2024-12-24 11:44:37 +0900

    Add: AutoAppConfig.java

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,2 +1,23 @@
-package hello.core;public class AutoAppConfig {
+package hello.core;
+
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.stereotype.Component;
+
+@Configuration
+@ComponentScan(
+        excludeFilters = @Filter(type = FilterType.ANNOTATION, classes = Configuration.class))
+
+public class AutoAppConfig {
+
+    @Bean(name = "memoryMemberRepository")
+    public MemoryMemberRepository memoryMemberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+
 }
